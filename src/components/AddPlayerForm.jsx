a7e2b561e19ee34450b959fd83907cb6ea5f8547
Geunhwan <cormsghks123@naver.com>
2020-10-31 12:18:35 +0900

    AddPlayerForm.jsx input을 uncontrolled component에서 controlled component로 변경

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 class AddPlayerForm extends React.Component{
   // class Component를 만들기 위해서는 반드시 render를 통해 element를 return해주어야한다.
+  state = {
+    value : ''
+  }
+  handleValueChange = (e) => {
+    this.setState({value: e.target.value});
+  }
   render() {
     return (
       <form className="form">
-        <input type="text" className="input" placeholder="enter a player's name"/>
+        <input type="text" className="input" placeholder="enter a player's name"
+          value={this.state.value}
+          onChange={this.handleValueChange}/>
         <input type="submit" className="input" value="Add Player"/>
       </form>
     );
