7f692f69a675e9f2e605a31c35980e30be1f47ec
Geunhwan <cormsghks123@naver.com>
2020-10-31 17:41:56 +0900

    App 컴포넌트에 hooks 적용완료

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import _ from 'lodash';
 let maxId = 4;
 
 function App(props) {
-  // 변수, set변수 = useState(초기값)
+// 변수, set변수 = useState(초기값)
   const [players, setPlayers] = useState([
     {name: 'Geunhwan', score: 0, id: 1},
     {name: 'Donghoon', score: 0, id: 2},
@@ -20,54 +20,47 @@ function App(props) {
   const handleRemovePlayer = (id) => {
     console.log('handleRemovePlayer:', id);
 
-    this.setState((prevState) => {
-
-      const players = prevState.players.filter((player) => player.id !== id);
-      return {players};
-    })
+    const newPlayers = players.filter((player) => player.id !== id);
+    setPlayers(newPlayers); // 비동기 실행된다.
   }
 
   const handleChangeScore = (delta, id) => {
     console.log('handleChangeScore:', delta, id);
-    this.setState((prevState) => {
-      const players = [...prevState.players];
-      players.forEach(player => {
-        if (player.id === id) {
-          player.score += delta;
-        }
-      })
-      return {players: players};
+    const newPlayers = [...players];
+    newPlayers.forEach(player => {
+      if (player.id === id) {
+        player.score += delta;
+      }
     })
+    setPlayers(newPlayers);
   }
 
   const handleAddPlayer = (name) => {
     console.log('handleAddPlayer : ', name);
-    this.setState(prevState => {
-      const players = [...prevState.players]
-      players.push({name: name, score: 0, id: ++maxId});
-      return {players};
-    })
+    const newPlayers = [...players]
+    newPlayers.push({name: name, score: 0, id: ++maxId});
+    setPlayers(newPlayers);
   }
 
   const getHighScore = () => {
     let maxHighScore = 0;
-    const maxObj = _.maxBy(this.state.players, 'score');
+    const maxObj = _.maxBy(players, 'score');
     return maxObj.score ? maxObj.score : null;
   }
 
   return (
     <div className='scoreboard'>
-      <Header title='My Scoreboard' players={this.state.players}/>
+      <Header title='My Scoreboard' players={players}/>
 
       {
-        this.state.players.map(player => (
+        players.map(player => (
           <CustomPlayer name={player.name} key={player.id} id={player.id} score={player.score}
-                        isHighScore={player.score === this.getHighScore()}
-                        changeScore={this.handleChangeScore}  // function의 결과값이 아닌 function 자체를 내려준다
-                        removePlayer={this.handleRemovePlayer}/>
+                        isHighScore={player.score === getHighScore()}
+                        changeScore={handleChangeScore}  // function의 결과값이 아닌 function 자체를 내려준다
+                        removePlayer={handleRemovePlayer}/>
         ))
       }
-      <AddPlayerForm addPlayer={this.handleAddPlayer}></AddPlayerForm>
+      <AddPlayerForm addPlayer={handleAddPlayer}></AddPlayerForm>
     </div>
   );
 }
